69eb928c86b38305ad477c328600c286a955e112
Seppi <josefnpat@hotmail.com>
2012-05-12 18:29:17 -0400

    Oops, need to config the build script

--- build.sh
+++ build.sh
@@ -13,11 +13,37 @@ cd ..
 # Temp Space
 mkdir tmp
 
-# WINDOWS
+# Linux 32 bit
+cp dev/build_data/love_i686/ tmp/ -r
+cp dev/build_data/run.sh tmp/
+cp ${NAME}_${GIT}.love tmp/
+cd tmp
+tar czvf ../${NAME}_linux-i686[$GIT].tar.gz *
+cd ..
+rm tmp/* -rf #tmp cleanup
+
+# Linux 64 bit
+cp dev/build_data/love_x86_64/ tmp/ -r
+cp dev/build_data/run.sh tmp/
+cp ${NAME}_${GIT}.love tmp/
+cd tmp
+tar czvf ../${NAME}_linux-x86_64[$GIT].tar.gz *
+cd ..
+rm tmp/* -rf #tmp cleanup
+
+# Windows 32 bit
 cat dev/build_data/love-$VERSION\-win-x86/love.exe ${NAME}_${GIT}.love > tmp/${NAME}_${GIT}.exe
 cp dev/build_data/love-$VERSION\-win-x86/*.dll tmp/
 cd tmp
-zip -r ../${NAME}_win_[$GIT].zip *
+zip -r ../${NAME}_win-x86[$GIT].zip *
+cd ..
+rm tmp/* -rf #tmp cleanup
+
+# Windows 64 bit
+cat dev/build_data/love-$VERSION\-win-x64/love.exe ${NAME}_${GIT}.love > tmp/${NAME}_${GIT}.exe
+cp dev/build_data/love-$VERSION\-win-x64/*.dll tmp/
+cd tmp
+zip -r ../${NAME}_win-x64[$GIT].zip *
 cd ..
 rm tmp/* -rf #tmp cleanup
 
@@ -26,7 +52,7 @@ cp dev/build_data/love.app tmp/${NAME}_${GIT}.app -Rv
 cp ${NAME}_${GIT}.love tmp/${NAME}_${GIT}.app/Contents/Resources/
 patch tmp/${NAME}_${GIT}.app/Contents/Info.plist -i dev/build_data/osx.patch
 cd tmp
-zip -r ../${NAME}_osx_[$GIT].zip ${NAME}_${GIT}.app
+zip -r ../${NAME}_macosx [$GIT].zip ${NAME}_${GIT}.app
 cd ..
 rm tmp/* -rf #tmp cleanup
 
--- build_dep_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd dev/build_data
-VERSION=0.8.0
-wget -c https://bitbucket.org/rude/love/downloads/love-$VERSION\-win-x86.zip
-unzip love-$VERSION\-win-x86.zip
-wget -c https://bitbucket.org/rude/love/downloads/love-$VERSION\-macosx-ub.zip
-unzip love-$VERSION\-macosx-ub.zip
